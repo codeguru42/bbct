f98c5da4889ba9facbe277cbd7da271bc420c9ce
Code Apprentice <codeguru42@gmail.com>
2016-09-19 16:54:02 -0600

    Create `MenuTest` and `LiteMenuTest`

---
+++ android/src/androidTest/java/bbct/android/common/activity/test/MenuTest.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of BBCT for Android.
+ *
+ * Copyright 2016 codeguru <codeguru@users.sourceforge.net>
+ *
+ * BBCT for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBCT for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bbct.android.common.activity.test;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import bbct.android.common.activity.MainActivity;
+
+@RunWith(AndroidJUnit4.class)
+public abstract class MenuTest<T extends MainActivity> {
+    @Rule
+    public ActivityTestRule<T> activityTestRule;
+
+    public MenuTest(Class<T> activityClass) {
+        activityTestRule = new ActivityTestRule<>(activityClass);
+    }
+
+    @Test
+    public void testAboutMenu() throws Exception {
+
+    }
+}
